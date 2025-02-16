d53a5affdd9ae4e5672250197a5464058cc5485d
muhamsh <muhammad-ashraful@outlook.com>
2025-02-16 15:13:24 +0600

    api calling refectored

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-
 import { auth } from '../../auth';
 import LandingPage from '../../components/LandingPage';
 import { fetchTaskStatusPercentage } from '../../utils/actions/tasks';
@@ -6,17 +5,15 @@ import { getUserInfo } from '../../utils/actions/user';
 
 export default async function HomePage() {
   const session = await auth();
+  const user = session?.user;
+  
   let userState = null;
   let getUser = null;
-  
-  if ( session?.user?.id )
-  {
-    const userState = await fetchTaskStatusPercentage( session?.user?.id );
-    const getUser = getUserInfo( session?.user?.id );
+
+  if (user?.id) {
+    userState = await fetchTaskStatusPercentage(user.id);
+    getUser = await getUserInfo(user.id);
   }
 
-  const user = session?.user;
-
-  // console.log( getUser );
-  return <LandingPage user={ user } getUser={getUser} userState={ userState } />
+  return <LandingPage user={user} getUser={getUser} userState={userState} />;
 }
